2f3c657ffca2fd6475b74f20cd49fcfbe6335103
King Bingus <bendawsdev@gmail.com>
2025-04-24 09:10:34 -0500

    thingy

--- Internals/CardboardNetwork.cs
+++ Internals/CardboardNetwork.cs
@@ -5,19 +5,7 @@ namespace Cardboard.Internals
 {
     public class CardboardNetwork : MonoBehaviourPunCallbacks
     {
-        public override void OnJoinedRoom()
-        {
-            base.OnJoinedRoom();
-
-            if (NetworkSystem.Instance.GameModeString.Contains("MODDED_"))
-                CardboardModded.CallModdedEvent(ModdedEventType.ModdedJoin);
-        }
-
-        public override void OnLeftRoom()
-        {
-            base.OnLeftRoom();
-
-            CardboardModded.CallModdedEvent(ModdedEventType.ModdedLeave);
-        }
+        public override void OnJoinedRoom() => NetworkSystem.Instance.GameModeString.Contains("MODDED_") ? CardboardModded.CallModdedEvent(ModdedEventType.ModdedJoin);
+        public override void OnLeftRoom() => CardboardModded.CallModdedEvent(ModdedEventType.ModdedLeave);
     }
 }
