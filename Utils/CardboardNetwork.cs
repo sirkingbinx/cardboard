2a303711f39b34d5e3a8fc9ec15eb736fcdbabb7
bingus <bendawsdev@gmail.com>
2025-08-29 14:38:51 -0500

    changes to cardboard network

--- Utils/CardboardNetwork.cs
+++ Utils/CardboardNetwork.cs
@@ -12,11 +12,39 @@ namespace Cardboard.Utils
         /// </summary>
         /// <param name="key">Key of the property to add.</param>
         /// <param name="value">Value of the property to add.</param>
-        public static void CreateProperty(string key, string value)
-        {
-            ExitGames.Client.Photon.Hashtable properties = new ExitGames.Client.Photon.Hashtable();
-            properties.Add(key, value);
-            PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
-        }
+        public static void CreateProperty(string key, string value) =>
+            PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable()
+            { {key, value} });
+
+        /// <summary>
+        /// Returns the properties of the local player.
+        /// </summary>
+        /// <returns>The custom properties list of the LocalPlayer.</returns>
+        public static Hashtable GetProperties() =>
+            PhotonNetwork.LocalPlayer.GetPlayerRef().CustomProperties;
+
+        /// <summary>
+        /// Returns the properties of the provided NetPlayer.
+        /// </summary>
+        /// <param name="player">The player to get the properties for.</param>
+        /// <returns>The custom properties list of the LocalPlayer.</returns>
+        public static Hashtable GetProperties(NetPlayer player) =>
+            player.GetPlayerRef().CustomProperties;
+        
+        /// <summary>
+        /// Returns the value of the provided key in the localplayer's properties.
+        /// </summary>
+        /// <param name="player">The player to get the properties for.</param>
+        /// <returns>Value of the key in properties</returns>
+        public static object GetPlayerProperty(string key) =>
+            PhotonNetwork.LocalPlayer.GetPlayerRef().CustomProperties[key];
+
+        /// <summary>
+        /// Returns the value of the provided key in the player's properties.
+        /// </summary>
+        /// <param name="player">The player to get the properties for.</param>
+        /// <returns>Value of the key in properties</returns>
+        public static object GetPlayerProperty(NetPlayer player, string key) =>
+            player.GetPlayerRef().CustomProperties[key];
     }
 }
